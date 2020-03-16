873f5c78afed62bdfc404716ad9bdaca512b3d12
Christian Barentine <cbarentine@gmail.com>
2020-03-16 14:27:25 -0600

    Cleared out old checkmarks function

--- redmark.js
+++ redmark.js
@@ -54,28 +54,3 @@ function GetPermittedPostingDate(redMarkObjects, redMarkCost) {
         return currentDate
     }
 }
-
-
-function CheckRedMarks(redMarkObjects, redMarkCost) { // unoptimized (should just run until you've found the latest outstanding red mark.)
-    let dayCounter = 0
-
-    let tempCounter = 0
-
-    let examiningDate = new Date()
-    let currentDate = new Date()
-
-    // let earliestOutstandingCost = new Date()
-
-    for (i=0; i < redMarkObjects.length; i++) {
-        if (examiningDate != redMarkObjects[i].date) {
-            diff = (currentDate - examiningDate.getTime()) / (1000 * 60 * 60 * 24)
-            tempCounter = Math.max(tempCounter - diff, 0) // make sure temp counter is > 0
-            dayCounter += tempCounter
-        }
-        examiningDate = redMarkObjects[i].date
-        tempCounter += redMarkCost
-    }
-
-    return dayCounter
-    
-}
