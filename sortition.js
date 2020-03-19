08564522c519150869ca4d857a4a06767d82a165
Christian Barentine <cbarentine@gmail.com>
2020-03-19 09:39:03 -0600

    made outline for invitation membership

--- sortition.js
+++ sortition.js
@@ -24,9 +24,35 @@ function MakeNewCouncil(name, members, membershipType, systemExclusive, terminat
     NotImplemented()
     // first, load all of the members in the community
     communityMembers = [] // placeholder for navigator.fileSystem.
+    availableMembers = [] // add possible members here based on system exclusive, and later, rotation.
+
+    // fill out the available members list
+    if (systemExclusive) {
+        occupiedMembers = [] // placeholder for navigator.fileSystem to find all members in currently active councils.
+        
+        for (const member in communityMembers) {
+            if (!occupiedMembers.includes(member)) {
+                availableMembers.push(member)
+            }
+        }
+
+    } else {
+        // then all community members are available.
+        availableMembers = communityMembers
+    }
     
+
     // check our membership type
     if (membershipType == MembershipType.INVITATION) {
+        // assume that members is a list of chosen members
+        for (const member in members) {
+            if (!availableMembers.includes(member)) {
+                throw {name: "InvalidMember", message: "The member " + member + " was not available."}
+            }
+        }
+
+        // membership checks out, create and return the new council.
+        return new Council(name, members, terminationCriteria)
 
     } else if (membershipType == MembershipType.ROTATION) {
 
@@ -36,7 +62,7 @@ function MakeNewCouncil(name, members, membershipType, systemExclusive, terminat
         throw {name: "InvalidMembershipType", message: "An invalid type was given to create a council. Use the MembershipType enum"}
     }
 
-    
+
 
 
 }
